95a4d9e590d7a6e01120d22146d13c39473f8124
Daniel Raskin <snowmanhat23@gmail.com>
2025-09-04 19:55:54 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@
 // This file contains all business branding information that can be easily changed for rebranding
 
 // Business Information
-export const BUSINESS_NAME = "Tenafly Chiropractic Center";
-export const BUSINESS_FULL_NAME = "Tenafly Chiropractic Center";
-export const BUSINESS_WEBSITE = "https://tenaflychiropracticcenter.com/";
-export const BUSINESS_DESCRIPTION = "Chiropractic";
+export const BUSINESS_NAME = "The Look Fitness";
+export const BUSINESS_FULL_NAME = "The Look Fitness";
+export const BUSINESS_WEBSITE = "https://thelookfitness.com/";
+export const BUSINESS_DESCRIPTION = "Fitness Facility";
 
 // Project Configuration
 export const PROJECT_NAME = "medspa-dashboard";
